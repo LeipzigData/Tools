2e7a3789e662e8691f78666803df773c22bf1594
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2013-09-12 18:34:33 +0200

    Änderungen durchgesehen und umformatiert

--- widget/getdata.php
+++ widget/getdata.php
@@ -14,66 +14,76 @@ include_once('db.php');
 $debug = true;
 if($debug)
 {
-	error_reporting(E_ALL);
-	ini_set('display_errors', true);
+  error_reporting(E_ALL);
+  ini_set('display_errors', true);
 }
 
 /**
  *	main
  */
  
-$local_store = false; /* set true to use the integrated store and if youre dumping data via turtle files (local or remote)
-						    * set false if you want to retrieve data directly from a (remote) sparql endpoint */
-
+$local_store = false; /* set true to use the integrated store and if youre
+		       * dumping data via turtle files (local or remote) set
+		       * false if you want to retrieve data directly from a
+		       * (remote) sparql endpoint */
 
 if ($local_store)
 {
-	/*initialize TripleStore */
-	$config = array(
-		/* db */
-		'db_name' => DB_NAME, 
-		'db_user' => DB_USER,
-		'db_pwd' => DB_PASSWORD,
-		/* store */
-		'store_name' => 'data_store',
-		/* stop after 100 errors */
-		'max_errors' => 100,
-	);
+  /*initialize TripleStore */
+  $config = 
+    array( 
+	  /* db */
+	  'db_name' => DB_NAME, 
+	  'db_user' => DB_USER,
+	  'db_pwd' => DB_PASSWORD,
+	  /* store */
+	  'store_name' => 'data_store',
+	  /* stop after 100 errors */
+	  'max_errors' => 100,
+	   );
 
-	// connect to store
-	$store = ARC2::getStore($config);
-	if (!$store->isSetUp() && $debug) 
-		print "\nNo store has been Found! You first have to configure your database settings in db_credentials.php and then import some triples using Store.php!\n";
-	else
-		if ($debug) print "\nsuccessfully connected to store\n";
+  // connect to store
+  $store = ARC2::getStore($config);
+  if (!$store->isSetUp() && $debug) 
+    print "\nNo store has been Found! You first have to configure your database settings in db_credentials.php and then import some triples using Store.php!\n";
+  else
+    if ($debug) print "\nsuccessfully connected to store\n";
 }
 else
 {
-	/* configuration */ 
-	$config = array(
-		'remote_store_endpoint' => 'http://leipzig-data.de:8890/sparql',
-	);
-	// connect to store
-	$store = ARC2::getRemoteStore($config);
+  /* configuration */ 
+  $config = 
+    array(
+	  'remote_store_endpoint' => 'http://leipzig-data.de:8890/sparql',
+	  );
+  // connect to store
+  $store = ARC2::getRemoteStore($config);
 }
 // filter data (run sparql query)
 $trips = filterData($store);
-if ($debug && $store->getErrors()) {print "\nErrors occured during sparql request:\n"; print_r($store->getErrors());}
+if ($debug && $store->getErrors()) 
+{
+  print "\nErrors occured during sparql request:\n"; 
+  print_r($store->getErrors());
+}
 if ($debug) print_r($trips);
-//$trips = ARC2::getTriplesFromIndex($trips);//TODO this is nonsense and decreases performance, but I don't know how to explain the plugin that the triples are resource indexed
+//$trips = ARC2::getTriplesFromIndex($trips);
+/* TODO this is nonsense and decreases performance, but I don't know how to
+explain the plugin that the triples are resource indexed */
 
 // write human readable json in debug mode
 if ($debug)
-	writeToFile(pretty_json(getExhibitJSON($trips,true)),'data.json');
+  writeToFile(pretty_json(getExhibitJSON($trips,true)),'data.json');
 else
-	writeToFile(getExhibitJSON($trips,true),'data.json');
+  writeToFile(getExhibitJSON($trips,true),'data.json');
 
 /**
  * this function retrieves data from the triple store via sparql
  * the query can be changed to filter the data as needed
  */
 function filterData($store) {
-  $q = 'CONSTRUCT {?s $p $o; $p $o.} WHERE { ?s ?p ?o}'; // query for local store (dumping all of it)
+  $q = 'CONSTRUCT {?s $p $o; $p $o.} WHERE { ?s ?p ?o}'; 
+  // query for local store (dumping all of it)
   // query for remote store, uncomment if using local store
   $q = '
 		PREFIX ld: <http://leipzig-data.de/Data/Model/>
@@ -97,34 +107,32 @@ function filterData($store) {
   $result = $store->query($q);
   global $debug;
   if ($debug)
-	echo 'imported '.count($result['result'])." triples\n";
+    echo 'imported '.count($result['result'])." triples\n";
   return $result['result'];
-}	
-	
+}      
 
 /**
  * writes data to a file
  */
 function writeToFile($contents,$filename)
 {
-	$fh = fopen($filename, 'w') or die("can't open file");
-	fwrite($fh, $contents);
-	fclose($fh);
+  $fh = fopen($filename, 'w') or die("can't open file");
+  fwrite($fh, $contents);
+  fclose($fh);
 }
-
 
 /**
  *	converts an array of arc2 triples into a serialized ExhibitJSON string
  */
 function getExhibitJSON($triples,$indextype)
 {
-	$config = array();
-	$my_ext = ARC2::getComponent('ARC2_ExhibitJSONSerializerPlugin', $config);
-	if($indextype)
-		$exhibitJSONtriples = $my_ext->getSerializedIndex($triples);
-	else
-		$exhibitJSONtriples = $my_ext->getSerializedTriples($triples);
-	return $exhibitJSONtriples;
+  $config = array();
+  $my_ext = ARC2::getComponent('ARC2_ExhibitJSONSerializerPlugin', $config);
+  if($indextype)
+    $exhibitJSONtriples = $my_ext->getSerializedIndex($triples);
+  else
+    $exhibitJSONtriples = $my_ext->getSerializedTriples($triples);
+  return $exhibitJSONtriples;
 }
 
 /**
@@ -133,53 +141,46 @@ function getExhibitJSON($triples,$indextype)
  */
 function pretty_json($json) {
 
-    $result      = '';
-    $pos         = 0;
-    $strLen      = strlen($json);
-    $indentStr   = '  ';
-    $newLine     = "\n";
-    $prevChar    = '';
-    $outOfQuotes = true;
+  $result      = '';
+  $pos         = 0;
+  $strLen      = strlen($json);
+  $indentStr   = '  ';
+  $newLine     = "\n";
+  $prevChar    = '';
+  $outOfQuotes = true;
 
-    for ($i=0; $i<=$strLen; $i++) {
+  for ($i=0; $i<=$strLen; $i++) {
 
-        // Grab the next character in the string.
-        $char = substr($json, $i, 1);
+    // Grab the next character in the string.
+    $char = substr($json, $i, 1);
 
-        // Are we inside a quoted string?
-        if ($char == '"' && $prevChar != '\\') {
-            $outOfQuotes = !$outOfQuotes;
+    // Are we inside a quoted string?
+    if ($char == '"' && $prevChar != '\\') {
+      $outOfQuotes = !$outOfQuotes;
         
-        // If this character is the end of an element, 
-        // output a new line and indent the next line.
-        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
-            $result .= $newLine;
-            $pos --;
-            for ($j=0; $j<$pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        // Add the character to the result string.
-        $result .= $char;
-
-        // If the last character was the beginning of an element, 
-        // output a new line and indent the next line.
-        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
-            $result .= $newLine;
-            if ($char == '{' || $char == '[') {
-                $pos ++;
-            }
-            
-            for ($j = 0; $j < $pos; $j++) {
-                $result .= $indentStr;
-            }
-        }
-        
-        $prevChar = $char;
+      // If this character is the end of an element, 
+      // output a new line and indent the next line.
+    } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+      $result .= $newLine;
+      $pos --;
+      for ($j=0; $j<$pos; $j++) { $result .= $indentStr; }
     }
+        
+    // Add the character to the result string.
+    $result .= $char;
 
-    return $result;
+    // If the last character was the beginning of an element, 
+    // output a new line and indent the next line.
+    if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+      $result .= $newLine;
+      if ($char == '{' || $char == '[') { $pos ++; }
+      for ($j = 0; $j < $pos; $j++) { $result .= $indentStr; }
+    }
+        
+    $prevChar = $char;
+  }
+
+  return $result;
 }
 
 ?>
--- widget/readme.txt
+++
@@ -1,4 +0,0 @@
-Installation entsprechend den Anweisungen in der Doku.
-
-Hier wird nun eine Datei data.json zentral vom Skript getdata.php erzeugt und
-dann auf alle Themen verteilt. 
