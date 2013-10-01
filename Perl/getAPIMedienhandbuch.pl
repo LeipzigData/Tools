5d39390d84cada86c9b4c6fd2cb6362be6858efd
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2013-10-01 17:48:11 +0200

    xx

--- Perl/getAPIMedienhandbuch.pl
+++ Perl/getAPIMedienhandbuch.pl
@@ -2,8 +2,9 @@ use strict;
 use APILeipzig;
 
 my $out=getPeople();
-$out.=getBranches();
-$out.=getCompanies();
+#$out.=getBranches();
+#$out.=getCompanies();
+
 print TurtleEnvelope($out);
 
 ## end main ##
@@ -11,7 +12,7 @@ print TurtleEnvelope($out);
 sub getPeople {
   my $u=APILeipzig::Query("http://www.apileipzig.de/api/v1/mediahandbook/people");
   my $out;
-  map $out.=process($_), @{$u->{data}};
+  map $out.=processPeople($_), @{$u->{data}};
   return $out;
 }
 
@@ -36,6 +37,8 @@ sub TurtleEnvelope {
 \@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
 \@prefix ld: <http://leipzig-data.de/Data/Model/> .
 \@prefix owl: <http://www.w3.org/2002/07/owl#> .
+\@prefix foaf: <http://xmlns.com/foaf/0.1/> .
+
 
 $out
 EOT
@@ -43,11 +46,30 @@ EOT
 
 # process data types
 
+sub processPeople {
+  my $a=shift;
+  my $id=$a->{id};
+  my $gname=$a->{first_name};
+  my $fname=$a->{last_name};
+  my $title=$a->{title};
+  my $occupation=$a->{occupation};
+  my $company=$a->{company_id};
+  my $uri=fixId($fname."_".$gname);
+  my $out;
+  $out.=addLiteral("foaf:givenName",$gname);
+  $out.=addLiteral("foaf:familyName",$fname);
+  $out.=addReference("ld:APIRef","<http://leipzig-data.de/Data/APIId/Person/$id>");
+  $out.=addLiteral("foaf:title",$title) if $title;
+  return<<EOT;
+<http://leipzig-data.de/Data/Person/$uri> a foaf:Person $out . 
+EOT
+}
+
 sub process {
   my $a=shift;
   my $id=$a->{id};
   my $out;
-  map { $out.=" $_ \"$a->{$_} \" ;\n"; } (sort keys %$a);
+  map { $out.=" ld:$_ \"$a->{$_}\" ;\n" if $a->{$_}; } (sort keys %$a);
   return "<http://leipzig-data.de/Data/$id> $out a ld:Uhu .\n\n";
 }
 
@@ -59,10 +81,19 @@ sub addReference { my ($a,$b)=@_; return " ;\n\t$a $b"; }
 sub fix {
   local $_=shift;
   return unless $_;
-  s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg; # unescape chars
-  s/\"/\\"/gs;
-  s/\+/ /gs;
-  s/\n/ <br\/> /gs;
-  s/\s+/ /gs;
+  s/ld:first_name/foaf:givenName/gs;
+  s/ld:last_name/foaf:familyName/gs;
+  s/ld:title/foaf:title/gs;
+  return $_;
+}
+
+sub fixId {
+  local $_=shift;
+  s|\s||g;
+  s|-||g;
+  s|ä|ae|g;
+  s|ö|oe|g;
+  s|ü|ue|g;
+  s|ß|ss|g;
   return $_;
 }
