b3c27b4c010e7b246b12361ca07ccf69ef912185
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-12-23 11:26:18 +0100

    Aktualisierung Events

--- Perl/getAPIEvents.pl
+++ Perl/getAPIEvents.pl
@@ -1,10 +1,11 @@
-# Aenderung 28.03.2015: time offset auf Sommerzeit gesetzt
-# Aenderung 28.10.2014: time offset auf Winterzeit gesetzt
+# Aenderung 23.12.2015: time offset in fixTime auf Winterzeit gesetzt
+# Aenderung 28.03.2015: time offset in fixTime auf Sommerzeit gesetzt
+# Aenderung 28.10.2014: time offset in fixTime auf Winterzeit gesetzt
 # Aenderung 06.08.2014: ld:hasURL zu ical:url
-# Aenderung 29.03.2014: time offset auf Sommerzeit gesetzt
+# Aenderung 29.03.2014: time offset in fixTime auf Sommerzeit gesetzt
 # Aenderung 19.03.2014: xsd:dateTime gefixt
 # Aenderung 09.02.2014: ld:contactPerson entfernt
-# Aenderung 29.10.2013: time offset auf Winterzeit gesetzt
+# Aenderung 29.10.2013: time offset in fixTime auf Winterzeit gesetzt
 # Aenderung 12.10.2013: time offset gefixt und Typangabe ergänzt
 # Aenderung 24.03.2013: ical:sentBy ergänzt
 
@@ -13,7 +14,7 @@ use APILeipzig;
 
 my $hash=APILeipzig::getTranslationHash();
 # map print("$_ -> $hash->{$_}\n"), (sort keys %$hash);
-print getEvents("2015-09-01");
+print getEvents("2015-11-01");
 
 ## end main ##
 
@@ -100,8 +101,8 @@ sub fixTime {
   s/2000-01-01//gs;
   s/:00:42/:00:00/gs;
   if (/T/) {
-    s/\+\d\d:\d\d/+02:00/;
-    $_.="+02:00" unless /\+/;
+    s/\+\d\d:\d\d/+01:00/;
+    $_.="+01:00" unless /\+/;
   }
   return $_;
 }
