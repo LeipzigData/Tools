5596fefe56442d3b011be06689c800e91900574d
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-12-27 10:51:11 +0100

    eVents nukla

--- Perl/stoiber.pl
+++ Perl/stoiber.pl
@@ -120,3 +120,176 @@ Exkursionsleiter: Andreas Liste, AHA e.V.
 Treff: Haus Auensee NUKLA e. V.
 EOT
 }
+
+sub getWevseite {
+  return <<EOT;
+14.1.16 Donnerstag, 17 – 18.30 Uhr
+Leipziger Floßgraben. Der Film. Teil II – Frühling
+(In Kooperation mit der Volkshochschule Leipzig)
+Ort: VHS Leipzig, Löhrstraße 3 – 7, Raum 301
+Kursnummer der Volkshochschule Leipzig: M 18 023 Z
+Eintritt frei
+----
+Der Film zeigt spannende 45 Minuten Vorfrühling im Einzugsbereich des Floßgrabens. Der Winter schwindet und der Frühling erblüht. Der Film beinhaltet etwas zur Entstehung des Floßgrabens und etwas zur aktuellen Problematik am Leipziger Floßgraben. Frühblüher, Vögel, Fische und Natur durch Naturstimmen und gesprochenem Text für Sie in Bildern festgehalten.
+
+31.01.16 Sonntag, 17 Uhr  
+25. Klassisches Kartoffelkonzert – „Mit ihm wäre ich überallhin gegangen…“ Die berührende Geschichte der Freundschaft von Tula und Maria Reemy mit Joachim Ringelnatz, recherchiert, gelesen und gesungen von Wolfgang Rieck
+Ort: Alte Börse Leipzig
+18 EUR Normalpreis, 15 EUR ermäßigt, 7 EUR Abendkasse mit LeipzigPass
+
+27.03.16 Sonntag, 17 Uhr
+26. Klassisches Kartoffelkonzert – Kammerorchester „Artentfaltung“  
+Leitung Stephan König
+Ort: Alte Börse Leipzig
+18 EUR Normalpreis, 15 EUR ermäßigt, 7 EUR Abendkasse mit LeipzigPass, ausgenommen Sonderkonzerte
+
+
+9.4.16 Sonnabend, 10 – 11.30 Uhr
+Frühlingsspaziergang durch den Auwald
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 121 Z
+Treff: Elsterbecken, Landauer Brücke, Hans-Driesch-Straße
+6 EUR (bereits ermäßigt)
+----
+Nach dem langen Winter freuen wir uns über das Vogelgezwitscher und die ersten Blumen. Wissen Sie immer, wer da singt und was da blüht? – Wie kommt eigentlich das Wasser in die Aue und wozu ist das wichtig? Ein vergnüglicher Vormittag mit Ralf Mäkert vom Nabu Leipzig, der Ihre Fragen gern beantwortet.
+
+16.4.16 Sonnabend, 18 – 20 Uhr
+Mythisches, Schauriges und Interessantes rund um den Leipziger Auwald und seine Bewohner
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 122 Z  
+Treff: Schleußiger Weg, Bushaltestelle Rennbahn/Eingang Schulbiologiezentrum  
+7 EUR, Kinder bis 12 Jahre sind in Begleitung eines Erwachsenen entgeltfrei, sie müssen aber angemeldet werden.
+----
+Der Auwald ist mehr als nur ein Wald. Heilige Bäume, Wassergeister, irreführende Gespenster und ehemalige Schlachtfelder sind hier zu finden. Dazu gibt es Interessantes über die Bewohner des Waldes – und natürlich auch ihre Gefährdung – an diesem Frühlingsabend zu hören. Empfehlenswert ist die Mitnahme einer Taschenlampe.
+Exkursionsleiter: Johannes Hansmann.
+
+23.4.16, Sonnabend, 14 – 15.30 Uhr 
+Exkursion zu den Frühjahrsblühern des Leipziger Auwaldes
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 130 Z 
+Treff: Teichstraße  
+6 EUR (bereits ermäßigt)
+----
+Unsere Tour führt vom Treffpunkt Teichstraße entlang der Linie durch den südlichen Auwald und endet am Schleußiger Weg. Besonders sehenswert sind auf dieser Exkursion die zahlreichen Frühjahrsblüher, die Ihnen Markus Kellermann zeigen wird.
+
+23.4.16 Sonnabend, 10 – 11.30 Uhr  
+Geführte Paddeltour im Drachenboot auf dem Karl-Heine-Kanal
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 131 Z 
+(Anmeldung erforderlich, letzter Rücktritt: 14.4.16) 
+17 EUR (bereits ermäßigt) 
+Treff: Bootsverleih Am Kanal 28
+----
+Eine Tour im Drachenboot ist zweifelsohne ein ganz besonderes Erlebnis. Die Standard-Sightseeing-Tour führt auf dem Karl-Heine-Kanal durch das ehemalige Industrieviertel Plagwitz. Vom Steuermann erhalten Sie eine Einführung in die Welt des Drachenboot-Sports und in die Paddeltechnik. Wir paddeln entlang der vielen ehemaligen Fabriken und Industriegebäude, welche heute mit Ihren renovierten und detailreichen Fassaden dem Viertel seinen ganz speziellen Charme verleihen. Auch unterqueren wir auf dieser Wasserstraße insgesamt 15 Brücken. Für Kinder bis 12 Jahre besteht Schwimmwestenpflicht. Die Schwimmwesten werden gestellt. Im Preis sind die Bootsmiete, Paddelmaterial und der Tourguide enthalten.
+Exkusionsleitung: Claudia Dietze, Dirk Hoffsky / Freizeit & Abenteuer.
+
+7.5.16, Sonnabend, 10 – 16 Uhr
+Fahrradexkursion in die Saale-Elster-Luppe-Aue zwischen Halle und Leipzig
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 120 Z  
+(Anmeldung erforderlich, letzter Rücktritt: 3.5.16)  
+Treff in Halle: Kreuzung Otto-Stomps-Straße / Reideburger Straße 
+10 EUR (bereits ermäßigt)
+----
+Die Anfahrt zum Treffpunkt ist mit der S-Bahn nach Halle um 8.30 Uhr möglich. Die Exkursion endet am Hauptbahnhof Leipzig.Die Tour führt uns entlang des Diemitzer Grabens, der Reide, zum Überlauf des Hufeisensees in die Reide, in den Park von Dieskau, durch die Saale-Elster-Luppe-Aue und entlang des Wallendorfer und des Raßnitzer Sees im Saalelandkreis, durch die Luppeaue bei Schkeuditz, durch die Elster-Luppe-Nahle-Aue in der Stadt Leipzig sowie zum Mündungsbereich der Parthe.
+Exkursionsleiter: Andreas Liste, AHA e.V.
+
+29.05.16, Sonntag,  17 Uhr 27. 
+Klassisches Kartoffelkonzert – „Wenn Purpur in den Wiesen lacht…“  
+Konzert mit barocker Musik voller Liebeslust und Frühlingsduft  mit dem Leipziger Barock-Consort
+Ort: Alte Börse Leipzig
+18 EUR Normalpreis, 15 EUR ermäßigt, 7 EUR Abendkasse mit LeipzigPass
+
+02.06.16, Donnerstag, 18 Uhr  
+3. AULA-Fachveranstaltung 
+Die „Aufgaben der Leipziger Aue“ von Naturschutz über Freizeit bis Tourismus? und Neues zum Floßgraben
+Ort: „City Tagung Leipzig“, 04109 Leipzig, Brühl 54, 4. Etage 
+Eintritt frei
+
+5.6.16, Sonntag 7 – 8.30 Uhr 
+Die Vogelwelt in der Leipziger Aue
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 150 Z 
+Treff: Schloss Lützschena, Schlossweg 9 
+6 EUR Ein Kind bis 12 Jahre ist in Begleitung eines Erwachsenen entgeltfrei, muss aber angemeldet werden.
+----
+Exkursion durch die Natur, um Vögel und ihre Lebensräume zu entdecken. Sie werden von einem Experten des NABU-Regionalverbandes Leipzig begleitet.Bitte tragen Sie bequeme Schuhe.
+
+25.6.16 Sonnabend, 21 – 23 Uhr 
+Grusel, Glühwürmchen und Fledermäuse – Johannisnachtexkursion durchs Rosental
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 152 Z  
+Treff: Rosentalgasse / Emil-Fuchs-Straße  
+7 EUR. Kinder bis 12 Jahre sind in Begleitung eines Erwachsenen entgeltfrei, sie müssen aber angemeldet werden.
+----
+Auch bei Nacht hat das Rosental seinen Reiz. Fledermäuse huschen durch die Baumwipfel und über die Flüsse. Glühwürmchen steigen in der Dämmerung aus dem Grün. Käuzchen und Eulen lassen sich mit ihren gruseligen Rufen hören. Und zu dieser Zeit sind die Waldkauzkinder besonders rege… Sogar Gespenster soll es hier geben… es gibt also nicht nur interessante Tiere zu sehen und zu hören, sondern auch allerlei Gruseliges und Schauriges rund um diese seit Jahrhunderten beliebte Park- und Waldlandschaft. August der Starke, Nixe und Räuber begleiten unseren düsteren Rundgang.
+Exkursionsleiter: Johannes Hansmann
+
+25.6.16 Sonnabend, 14 – 16 Uhr
+Kleine Hafentour für Paddelanfänger
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 154 Z 
+(Anmeldung erforderlich, letzter Rücktritt: 16.6.16)
+Treff: Bootsverleih Am Kanal 28  
+22 EUR (bereits ermäßigt)
+----
+Seit Juli 2015 ist Leipzig um ein paar Kilometer an paddelbaren Gewässern reicher. Jetzt hat auch der Lindenauer Hafen endlich die langersehnte Wasseranbindung. Die kleine Hafentour geht zum Lindenauer Hafen. Es ist eine kurze Tour, welche das Kennenlernen des Wasserwanderns mit Wissenswertem zur Entstehungsgeschichte des Kanals kombiniert. Die Hafentour ist hervorragend für Anfänger geeignet. Im Preis sind die Bootsmiete, Paddelmaterial und der Tourguide enthalten. Für Kinder bis 12 Jahre besteht Schwimmwestenpflicht. Die Schwimmwesten werden gestellt.
+Exkursionsleitung: Claudia Dietze, Dirk Hoffsky / Freizeit & Abenteuer.
+
+7.8. Sonntag, 9.30 – 14.30 Uhr
+Kanutour Cospudener See
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: N 18 156 Z 
+(Anmeldung erforderlich, letzter Rücktritt: 14.7.16) 
+Treff: Bootsverleih Am Kanal 28  
+35 EUR (bereits ermäßigt)
+----
+Unsere Tour beginnt mit einer kurzen Einweisung in das „Paddel-Ein-mal-eins“. Wir bewegen uns auf einigen recht unterschiedlichen Wasserwegen Leipzigs. Idyllisch schlängelt sich der Floßgraben durch den Auwald. Unsere Tour geht über die Pleiße – Elsterflutbett – Weiße Elster und schlussendlich auf den Karl Heine Kanal.  Im Preis sind die Bootsmiete, Paddelmaterial, Personentransfer und der Tourguide enthalten. Für Kinder bis 12 Jahre besteht Schwimmwestenpflicht. Die Schwimmwesten werden gestellt.
+Exkursionsleitung: Claudia Dietze, Dirk Hoffsky / Freizeit & Abenteuer.
+
+22.09.16 Donnerstag, 19.30 Uhr 
+28. Klassisches Kartoffelkonzert – Thomas Hanke und Plaintive Cry mit Chamber Blues
+Ort: Jazzkeller im Telegraph  
+18 EUR Normalpreis, 15 EUR ermäßigt, 7 EUR Abendkasse mit LeipzigPass
+
+24.9.16 Sonnabend, 10 – 15 Uhr 
+Entlang der Parthe / Fahrradexkursion von Taucha nach Leipzig
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: O 18 110 Z 
+(Anmeldung erforderlich, letzter Rücktritt am 20.9.)  
+Treff: 10:00 Uhr Bahnhof Taucha
+10 EUR (bereits ermäßigt)
+----
+„Die Parthe durch Stadt und Land“ – so folgen wir dem Flüsschen von Taucha nach Leipzig bis zur Mündung in die Weiße Elster.
+Exkursionsleiter: Andreas Liste, AHA e.V.
+
+5.11.16 Sonnabend, 10 – 14 Uhr
+Herbstexkursion in die Pleiße-Elster-Aue
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: O 18 132 Z 
+Treff: 10:00 Uhr, Bus 70, Haltestelle „Koburger Brücke“.  
+Die Exkursion endet am Leipziger Hauptbahnhof.  
+8 EUR (bereits ermäßigt)
+----
+Erleben und genießen Sie den Herbst im südlichen Auwald – das Licht, die Gerüche und Geräusche.
+Exkursionsleiter: Andreas Liste, AHA e.V.
+
+26.11.16 Sonnabend, 10 – 14 Uhr 
+Herbstexkursion zur Burgaue in Leipzig
+(In Kooperation mit der Volkshochschule Leipzig)
+Kursnummer der Volkshochschule Leipzig: O 18 134 Z 
+Treff: Haus Auensee  8 EUR (bereits ermäßigt)
+----
+Exkursionsleiter: Andreas Liste, AHA e.V.
+
+26.11.16 Sonnabend, 19.30 Uhr  
+29. Klassisches Kartoffelkonzert – Uschi Brüning und Band
+Ort: Culturhaus Stötteritz
+Eintritt lt. Plakataushang
+
+27.11.16 Sonntag, 14.30 und 20 Uhr  
+30. Klassisches Kartoffelkonzert – Doppelkonzert zum 1. Advent mit amarcord und Dorothee Oberlinger
+Ort: Paul-Gerhardt-Kirche, Leipzig-Connewitz
+Eintritt lt. Plakataushang
+EOT
+}
