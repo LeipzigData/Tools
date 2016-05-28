b76a23436aa42bc2f2270c98252f816d7e7e9804
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2016-05-28 19:22:27 +0200

    Weitere Arbeit an der Datentransformation

--- leipziger-ecken/direct/main.php
+++ leipziger-ecken/direct/main.php
@@ -9,7 +9,7 @@ include_once("adressen.php");
 include_once("events.php");
 include_once("sparten.php");
 
-//file_put_contents ("Daten/Akteure.ttl",getAkteure()); 
-//file_put_contents ("Daten/Adressen.ttl",getAdressen()); 
+file_put_contents ("Daten/Akteure.ttl",getAkteure()); 
+file_put_contents ("Daten/Adressen.ttl",getAdressen()); 
 file_put_contents ("Daten/Events.ttl",getEvents());  
 file_put_contents ("Daten/Sparten.ttl",getSparten()); 
