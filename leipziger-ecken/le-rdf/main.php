27c9b62acb79261628d0edc5d5bc2025290cdec6
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2017-10-26 17:58:35 +0200

    directory structure adjusted

--- leipziger-ecken/le-rdf/main.php
+++ leipziger-ecken/le-rdf/main.php
@@ -13,14 +13,14 @@ include_once("sparten.php");
 main();
 
 function main() {
-  file_put_contents ("Daten/Akteure.ttl",toRDFString(getAkteure())); 
-  echo "<p>Ausgabe Daten/Akteure.ttl erzeugt</p> \n";
-  file_put_contents ("Daten/Adressen.ttl",toRDFString(getAdressen())); 
-  echo "<p>Ausgabe Daten/Adressen.ttl erzeugt</p> \n";
-  file_put_contents ("Daten/Events.ttl",toRDFString(getEvents()));  
-  echo "<p>Ausgabe Daten/Events.ttl erzeugt</p> \n";
-  file_put_contents ("Daten/Sparten.ttl",toRDFString(getSparten())); 
-  echo "<p>Ausgabe Daten/Sparten.ttl erzeugt</p> \n";
+  file_put_contents ("../Daten/Akteure.ttl",toRDFString(getAkteure())); 
+  echo "<p>Ausgabe ../Daten/Akteure.ttl erzeugt</p> \n";
+  file_put_contents ("../Daten/Adressen.ttl",toRDFString(getAdressen())); 
+  echo "<p>Ausgabe ../Daten/Adressen.ttl erzeugt</p> \n";
+  file_put_contents ("../Daten/Events.ttl",toRDFString(getEvents()));  
+  echo "<p>Ausgabe ../Daten/Events.ttl erzeugt</p> \n";
+  file_put_contents ("../Daten/Sparten.ttl",toRDFString(getSparten())); 
+  echo "<p>Ausgabe ../Daten/Sparten.ttl erzeugt</p> \n";
 
 
 }
