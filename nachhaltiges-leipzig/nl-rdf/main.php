c3559908ea6da502bd0896de7adc881a2afc8453
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2018-08-05 17:40:04 +0200

    Dumps aktualisiert

--- nachhaltiges-leipzig/nl-rdf/main.php
+++ nachhaltiges-leipzig/nl-rdf/main.php
@@ -16,6 +16,7 @@ function save($method,$file) {
    
 function main() {
     save("getAkteure","Akteure.ttl"); 
+    save("getLDAkteure","LD-Akteure.ttl"); 
     save("getPersonen","Personen.ttl"); 
     save("getAllActivities","Activities.ttl"); 
 }
